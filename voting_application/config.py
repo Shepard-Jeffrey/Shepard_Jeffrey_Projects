071a83f21af52478c541bec839e679227d6a882e
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-05-19 01:41:26 -0400

    made deletions

--- voting_application/config.py
+++
@@ -1,9 +0,0 @@
-#configuration file for votr 
-import os
-
-DB_PATH = os.path.join(os.path.dirname(__file__), 'app.db')
-SECRET_KEY = 'development key' # keep this key secret during production 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(DB_PATH)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = True
-
