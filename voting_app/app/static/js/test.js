f14ad23eb056496b86e7c0b706430cefe0580649
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-05-19 01:38:16 -0400

    changed voting app name

---
+++ voting_app/app/static/js/test.js
@@ -0,0 +1,131 @@
+// // css style to align text to the center of it's container
+// var Align = {
+//     textAlign: 'center',
+//     fontFamily: 'EB Garamond'
+//   };
+
+// var origin = window.location.origin;
+
+// var Router = ReactRouter.Router;
+// var Route = ReactRouter.Route;
+// var browserHistory = ReactRouter.browserHistory;
+
+//   var PollForm = React.createClass({
+  
+//     getInitialState: function(){
+//       // set initial state of form inputs
+//       return {title: '', option: '', options: []}
+//     },
+  
+//     handleTitleChange: function(e){
+//       //change title as the user types
+//       this.setState({title: e.target.value});
+//     },
+  
+//     handleOptionChange: function(e){
+//       this.setState({option: e.target.value});
+//     },
+  
+//     handleOptionAdd: function(e){
+//       //update poll options and reset options to an empty string
+//       this.setState({
+//       options: this.state.options.concat({name: this.state.option}),
+//       option: ''
+//       });
+//     },
+
+//     handleSubmit: function(e){
+//       console.log("It submitted");
+//       e.preventDefault();
+//       var title = this.state.title;
+//       var options = this.state.options;
+
+//       var data = {'title': title, options: options.map(function(x){return x.name})};
+//       var url =  origin + '/api/polls'
+
+//     // make post request
+//       $.ajax({
+//         url: url,
+//         dataType: 'json',
+//         type: 'POST',
+//         data: JSON.stringify(data),
+//         contentType: 'application/json; charset=utf-8',
+//         success: function(data){
+//           alert(data.message);
+//         }.bind(this),
+//         error: function(xhr, status, err){
+//           alert('Poll creation failed: ' + err.toString());
+//         }.bind(this)
+//         });
+//     },
+  
+//     render: function(){
+//       return (
+//       <div>
+//         <form id="poll_form" className="form-signin" onSubmit={this.handleSubmit}>
+//           <h2 className="form-signin-heading" style={Align}>Create Election</h2>
+  
+//           <div className="form-group has-success">
+//             <label htmlFor="title" className="sr-only">Name of the Election</label>
+//             <input type="text" id="title" name="title" className="form-control" placeholder="Title" onChange={this.handleTitleChange} required autoFocus />
+//           </div>
+  
+//           <div className="form-group has-success">
+//             <label htmlFor="option" className="sr-only">Candidate</label>
+//             <input type="text" id="option" name="option" className="form-control" placeholder="Option" onChange={this.handleOptionChange}
+//             value={this.state.option ? this.state.option: ''} required autoFocus />
+//           </div>
+
+//           <div className="row form-group">
+//             <button className="btn btn-lg btn-success btn-block" type="button" onClick={this.handleOptionAdd}>Add Candidate</button>
+//             <button className="btn btn-lg btn-success btn-block" type="submit" onClick={this.handleSubmit}>Dead Election</button>
+//           </div>
+//           <br />
+//         </form>
+  
+//         <h3 style={Align}>Live Preview</h3>
+//         <LivePreview title={this.state.title} options={this.state.options} />
+//       </div>
+//       );
+//     }
+//   });
+  
+//   var LivePreview = React.createClass({
+//     render: function(){
+  
+//       var options = this.props.options.map(function(option){
+  
+//         if(option.name) {
+//           return (
+//             <div key={option.name}>
+//               <input name="options" type="radio" value={option.name} /> {option.name}
+//               <br />
+//             </div>
+//           );
+//         }
+//       });
+  
+//       return(
+//         <div className="panel panel-success">
+//           <div className="panel-heading">
+//             <h4>{this.props.title}</h4>
+//           </div>
+//           <div className="panel-body">
+//             <form>
+//               {options}
+//               <br />
+//               <button type="submit" className="btn btn-success btn-outline hvr-grow">Vote!</button>
+//             </form>
+//           </div>
+//         </div>
+//       )
+//     }
+//   });
+  
+//   ReactDOM.render((
+//     <Router history={browserHistory}>
+//       <Route path="/polls" component={PollForm} /> 
+//     </Router> 
+//     ),
+//     document.getElementById('container')
+//   );
